7be8a2d9a0a0f94d79c93fdf92c131b6732a8670
Claudia Fox <claudiafox95@icloud.com>
2018-11-13 11:34:19 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- i = 0 
+  i = 0
+  new_collection = []
+    while i < collection.size
+      if yield(collection[i]) == true
+        new_collection.push collection[i]
+      end
+      i += 1
+    end
+    new_collection
+  end	
 end
